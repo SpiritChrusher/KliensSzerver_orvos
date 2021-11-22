be728fc23db4e18ab1802fd30c2aca76968c948d
Kristof Szabo <Kristof.Szabo@logmein.com>
2021-11-22 19:41:33 +0100

    Assistant client project created

--- KliensSzerver_orvos/Migrations/20211122171958_Second.Designer.cs
+++ KliensSzerver_orvos/Migrations/20211122171958_Second.Designer.cs
@@ -3,6 +3,7 @@ using KliensSzerver_orvos.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KliensSzerver_orvos.Migrations
 {
     [DbContext(typeof(PatientContext))]
-    partial class PatientContextModelSnapshot : ModelSnapshot
+    [Migration("20211122171958_Second")]
+    partial class Second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
