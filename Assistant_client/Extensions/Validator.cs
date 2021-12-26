6e041e5902f7e8517b80522c5de448fb462a392d
Kristof Szabo <Kristof.Szabo@logmein.com>
2021-12-26 13:18:29 +0100

    validation update

--- Assistant_client/Extensions/Validator.cs
+++ Assistant_client/Extensions/Validator.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace Assistant_client.Extensions;
 
 public static class Validator
 {
-    public static bool ValidateName(string name)
-    {
-        
-    }
+    public static bool IsValidName(string name) =>
+        name.Equals(String.IsNullOrEmpty) || name.Equals(String.Empty)
+        || name.Any(s => ! Char.IsLetter(s)) ? false : true;
+
+    public static bool IsValidSSN(string ssn) => 
+        Regex.IsMatch(ssn, "^[0-9]{3} [0-9]{3} [0-9]{3}$");
+    
 }
