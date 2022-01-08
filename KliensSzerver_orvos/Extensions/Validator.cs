5b85f9ef5f666cdfb51147478515bcec6926051e
Kristof Szabo <Kristof.Szabo@logmein.com>
2022-01-08 01:36:57 +0100

    CreatePatient Page and validation fix

--- KliensSzerver_orvos/Extensions/Validator.cs
+++ KliensSzerver_orvos/Extensions/Validator.cs
@@ -6,18 +6,16 @@ public static class Validator
     {
         return request.Name.Equals(string.Empty) || request.Address.Equals(string.Empty)
             || request.SSN.Equals(string.Empty) || request.Description.Equals(string.Empty)
-            || string.IsNullOrWhiteSpace(request.Name) || string.IsNullOrWhiteSpace(request.Address)
-            || string.IsNullOrWhiteSpace(request.SSN) || string.IsNullOrWhiteSpace(request.Description)
-            || request.SSN.Length > 12;
+            || string.IsNullOrWhiteSpace(request.Name) || string.IsNullOrWhiteSpace(request.SSN)
+            || string.IsNullOrWhiteSpace(request.Description) || request.SSN.Length > 12;
     }
 
     public static bool IsPatientDtoBad(PatientDto request)
     {
         return request.Id <= 0 || request.Name.Equals(string.Empty) || request.Address.Equals(string.Empty)
             || request.SSN.Equals(string.Empty) || request.Description.Equals(string.Empty) 
-            || string.IsNullOrWhiteSpace(request.Name) || string.IsNullOrWhiteSpace(request.Address)
-            || string.IsNullOrWhiteSpace(request.SSN) || string.IsNullOrWhiteSpace(request.Description)
-            || request.SSN.Length > 12;
+            || string.IsNullOrWhiteSpace(request.Name) || string.IsNullOrWhiteSpace(request.SSN) 
+            || string.IsNullOrWhiteSpace(request.Description) || request.SSN.Length > 12;
     }
 
     public static bool ValidateLong(long id) => id <= 0;
